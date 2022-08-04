0b41920a36d30a6ba64245b1a3bc9603b788a1af
SvenHoeffler <shoeffler@wice.de>
2022-08-04 16:34:39 +0200

    Added API functions

--- test/utils.test.js
+++ test/utils.test.js
@@ -2,82 +2,65 @@
 
 const { expect } = require('chai');
 const nock = require('nock');
-const { getAccessToken, getObjects, upsertObject } = require('./../lib/utils/helpers');
-
-const dummyAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9';
+const { getContacts, createContact } = require('../lib/utils/helpers');
 
 describe('Helpers', () => {
-  it('should fetch access token', async () => {
-    nock('https://secret-service.openintegrationhub.com')
-      .get('/secrets/5d4c052dca2bda001072684c')
-      .reply(200, { value: { accessToken: dummyAccessToken } });
-
-    const cfg = {
-      secret: '5d4c052dca2bda001072684c',
-    };
-    const token = await getAccessToken(cfg);
-    console.log('Token received', token);
-    expect(token).to.equal(dummyAccessToken);
-  });
-
-  it('should get Objects, filtered by timestamp', async () => {
-    nock('https://people.googleapis.com', { reqheaders: { authorization: `Bearer ${dummyAccessToken}` } })
-      .filteringPath(() => '/v1/people/me/connections')
-      .get('/v1/people/me/connections')
+  it('should get contacts filtered by list', async () => {
+    nock('https://api.sendfox.com/contacts')
+      .get('')
+      .query({
+        page: 1,
+      })
       .reply(200, {
-
-        connections: [
-          {
-            firstName: 'Jane',
-            lastName: 'Doe',
-            lastUpdate: '2020-10-26T15:44:10+0000',
-          },
-          {
-            firstName: 'Somebody',
-            lastName: 'Else',
-            lastUpdate: '2019-10-26T15:44:10+0000',
-          },
-          {
-            firstName: 'Nobody',
-            middleName: 'of',
-            lastName: 'Note',
-            lastUpdate: '2018-10-26T15:44:10+0000',
-          },
-        ],
-
+        last_page: 2,
+        data: [{
+          id: 123, email: 'test@best.de', first_name: 'Jane', last_name: 'Doe', lists: [{ id: 370 }, { id: 159 }],
+        }],
       });
 
-    const { objects, newSnapshot } = await getObjects(dummyAccessToken, {});
-    console.log('Objects fetched', objects);
-    console.log(newSnapshot);
-    expect(objects).to.have.lengthOf(3);
+    nock('https://api.sendfox.com/contacts')
+      .get('')
+      .query({
+        page: 2,
+      })
+      .reply(200, {
+        last_page: 2,
+        data: [{
+          id: 456, email: 'test@bestest.de', first_name: 'Janey', last_name: 'Doey',
+        },
+        {
+          id: 789, email: 'some@body.de', first_name: 'Another', last_name: 'Person', lists: [{ id: 159 }],
+        }],
+      });
+
+    const contacts = await getContacts('someToken', {}, 159);
+
+    expect(contacts.length).to.equal(2);
+    expect(contacts[0].email).to.equal('test@best.de');
+    expect(contacts[1].email).to.equal('some@body.de');
   });
 
-  xit('should dynamically insert an object', async () => {
-    nock('https://api.example.com', { reqheaders: { authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9' } })
-      .post('/', { firstName: 'New', lastName: 'Entry' })
-      .reply(201, { id: '4711' });
+  it('should create a new contact', async () => {
+    nock('https://api.sendfox.com/contacts')
+      .post('', {
+        first_name: 'Jane',
+        last_name: 'Doe',
+        email: 'jdoe@doecorp.com',
+        lists: [962],
+      })
+      .reply(200, {
+        id: 369,
+      });
 
+    const newContact = {
+      first_name: 'Jane',
+      last_name: 'Doe',
+      email: 'jdoe@doecorp.com',
+    };
 
-    const { success, responseId } = await upsertObject({ firstName: 'New', lastName: 'Entry' }, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9');
+    const { success, responseId } = await createContact(newContact, 'someToken', false, 962);
 
-    expect(success).to.be.true;
-    expect(responseId).to.equal('4711');
-  });
-
-  xit('should dynamically update an object', async () => {
-    nock('https://api.example.com', { reqheaders: { authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9' } })
-      .get('/4711')
-      .reply(200, { id: '4711' });
-
-    nock('https://api.example.com', { reqheaders: { authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9' } })
-      .put('/4711', { firstName: 'Updated' })
-      .reply(200, { id: '4711' });
-
-
-    const { success, responseId } = await upsertObject({ firstName: 'Updated' }, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9', '4711');
-
-    expect(success).to.be.true;
-    expect(responseId).to.equal('4711');
+    expect(success).to.equal(true);
+    expect(responseId).to.equal(369);
   });
 });
